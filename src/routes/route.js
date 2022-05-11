04ce40de3aff66c3385b2e166a4812ccefa10816
bkrajor <bkrajor@gmail.com>
2022-05-12 00:47:44 +0530

    inProgress

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,7 +4,8 @@ const userController = require('../controllers/userController')
 const bookController = require('../controllers/bookController')
 const { authenticate, authorize } = require('../middleware/auth')
 
-router.post('/register', userController.createUser)
+
+router.post('/register', userController.createUser) 
 
 router.post('/login', userController.userlogin)
 
@@ -14,9 +15,11 @@ router.get('/books',authenticate, bookController.getBooks)
 
 router.get('/books/:bookId',authenticate, bookController.getBookById)
 
+router.put('/books/:bookId', authenticate, authorize, bookController.updateBook)
+
 router.delete('/books/:bookId', authenticate, authorize, bookController.deleteBookById)
 
-router.put('/books/:bookId', authenticate, authorize, bookController.updateBook)
+
 
 
 module.exports = router; 
